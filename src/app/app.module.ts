70a0f78d8d3f35c8f07962a830a9dee3027b7de9
chjuca <chjuca@utpl.edu.ec>
2021-01-07 18:20:07 -0500

    Add resources folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResourceFormComponent } from './components/resource-form/resource-form.component';
+import { ResourcesComponent } from './components/resources/resources.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResourceFormComponent,
+    ResourcesComponent
   ],
   imports: [
     BrowserModule,
