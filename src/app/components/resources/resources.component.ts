90f6fd3321a2312f39da223fdbd9d28f2ee61923
chjuca <chjuca@utpl.edu.ec>
2021-01-08 01:10:41 -0500

    Add services in the resources list view

--- src/app/components/resources/resources.component.ts
+++ src/app/components/resources/resources.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Resource } from './../../models/resources.interface';
+import { ResourceService } from './../../services/resource.service';
+import { Component, TemplateRef, OnInit } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-resources',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResourcesComponent implements OnInit {
 
-  constructor() { }
+  modalRef: BsModalRef;
+  resources = [];
+  editingResource: Resource;
+  resource: Resource;
+
+  constructor(private modalService: BsModalService, public resourceService: ResourceService) { }
 
   ngOnInit() {
+    this.resourceService.getResources().subscribe(resources => {
+      this.resources = resources
+    });
+  }
+
+  deleteResource() {
+    this.resourceService.deleteResource(this.resource);
+  }
+
+  updateResource() {
+    this.resourceService.updateResource(this.editingResource);
+    this.editingResource = {} as Resource;
+
+  }
+  openModal(template: TemplateRef<any>, resource: Resource) {
+    this.resource = resource;
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
+  }
+
+  confirm(): void {
+    this.deleteResource();
+    this.modalRef.hide();
+  }
+
+  decline(): void {
+    this.modalRef.hide();
   }
 
 }
